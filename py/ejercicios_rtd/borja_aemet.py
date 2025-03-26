62000d41b839d1eb9a3937393f57202759bf34a5
borjalopezfernandez92 <borjalopezfernandez92@gmail.com>
2025-03-26 11:15:02 +0100

    ejercicio aemete final

--- py/ejercicios_rtd/borja_aemet.py
+++ py/ejercicios_rtd/borja_aemet.py
@@ -19,12 +19,28 @@ responseData = requests.request("GET", urlData, headers=headers, params=querystr
 
 df = pd.DataFrame(responseData)                                                                     # Creo un dataframe con los datos
 getafe = df[df['nombre'].str.contains('GETAFE', case=False)]                                        # Y busco la que contiene getafe
-getafeCode = getafe.iloc[0]['indsinop']                                                             # Busco su código
+getafeCode = getafe.iloc[0]['indicativo']                                                             # Busco su código
 
 firstDate = '2024-08-22T08%3A30%3A30UTC'                                                            # Preparo las fechas de búsqueda
 secondDate = '2024-08-23T08%3A30%3A30UTC'                                                           #   "             "         "
 
 urlGetafe = f'https://opendata.aemet.es/opendata/api/valores/climatologicos/diarios/datos/fechaini/{firstDate}/fechafin/{secondDate}/estacion/{getafeCode}' #Preparo la URL para obtener las temperaturas
 
-responseGetafe = requests.request("GET", urlGetafe, headers=headers)    # y me las traigo
-print(f"Esta es la respuesta del último endpoint {responseGetafe} pero parece estar vacía: ",responseGetafe.text) # pero esta respuesta parece vacía
+responseGetafe = requests.request("GET", urlGetafe, headers=headers, params=querystring).json()    # y me las traigo
+
+urlFinalData = responseGetafe['datos']                                                             # Accedo a la url que recibo
+
+finalData = requests.request("GET", urlFinalData, headers=headers).json()                          # Y pido los datos que corresponden a las fechas y estación correspondientes
+
+print("┌","-"*40,"┐")
+
+print("  Q1-> ¿Cuantas requests os hacen falta hacer?")
+print("  R1 -> 2 Requests para obtener los datos de getafe.\n4 Para obtener la media de las temperaturas")
+print(" ","-"*40)
+print("  Q2-> ¿Cómo vais a meter getafe?")
+print("  R2 -> Realizo un request a la url 'urlEstaciones' la cual me devuelve otra url. Al hacer un request a ésta y filtrar los resultados por Getafe, recibo el código que le corresponde bajo la clave 'indicativo'.")
+print(" ","-"*40)
+print("  Q3 -> ¿cuál es la request final?")
+print("  R3 -> 'finalData' en mi código. Realizo una petición al enlace que contiene los datos meteorológicos de Getafe en las fechas correspondientes")
+print(f"  La temperatura media es: {finalData[0]['tmed']}º")                                         # Resultado final
+print("└","-"*40,"┘")
